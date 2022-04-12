625d88e836df2ed276ce2e5631b6555e660cc38f
fawda123 <mbafs2012@gmail.com>
2022-04-12 14:59:39 -0400

    R archive build script done

---
+++ R/archive.R
@@ -0,0 +1,78 @@
+library(gh)
+library(aws.s3)
+library(magrittr)
+
+# aws keys
+access_key <- Sys.getenv('AWS_ACCESS_KEY_ID')
+secret_key <- Sys.getenv('AWS_SECRET_ACCESS_KEY')
+Sys.setenv(
+  "AWS_ACCESS_KEY_ID" = access_key,
+  "AWS_SECRET_ACCESS_KEY" = secret_key
+)
+
+# identify repos in tbep-tech, except this one
+repos <- gh::gh(
+  "/orgs/{org}/repos",
+  org = "tbep-tech",
+  type = "all",
+  per_page = 100,
+  .limit = Inf
+) %>% 
+  purrr::map_chr("name") %>% 
+  .[!. %in% 'github-backup']
+
+handle <- curl::handle_setheaders(
+  curl::new_handle(followlocation = FALSE), 
+  "Authorization" = paste("token", Sys.getenv("GITHUB_PAT")),
+  "Accept" = "application/vnd.github.v3+json"
+)
+
+get_migration_state <- function(migration_url) {
+  status <- gh::gh(migration_url)
+  status$state
+}
+
+str <- Sys.time()
+
+# archive and upload to s3 for each repo
+for(i in seq_along(repos)){
+  
+  repo <- repos[i]
+  
+  # counter
+  msg <- paste0(repo, ', ', i, ' of ', length(repos), '\n')
+  cat(msg)
+  print(Sys.time() - str)
+  
+  # setup and download archive of repo as .tar.gz
+  migration <- gh::gh(
+    "POST /orgs/{org}/migrations",
+    org = "tbep-tech",
+    repositories = as.list(repo)
+  )
+  
+  migration_url <- migration[["url"]]
+
+  while (get_migration_state(migration_url) != "exported") {
+    cat("\tWaiting for export to complete...\n")
+    Sys.sleep(60)
+  }
+  
+  url <- sprintf("%s/archive", migration_url)
+  req <- curl::curl_fetch_memory(url, handle = handle)
+  headers <- curl::parse_headers_list(req$headers)
+  final_url <- headers$location
+  file_path <- sprintf("%s_migration_archive.tar.gz", repo) 
+  curl::curl_download(
+    final_url, 
+    file_path
+  )
+ 
+  # upload to S3
+  cat('\tUpload to S3...\n')
+  put_object(file_path, bucket = 'tbep-tech-github-backup')
+
+  # remove local file
+  file.remove(file_path)
+  
+}
--- README.md
+++ README.md
@@ -4,5 +4,5 @@
 <!-- badges: start -->
 <!-- badges: end -->
 
-This repository is used to create an archive of all repositories in the tbep-tech GitHub organization page and upload to Amazon S3 services on a weekly basis.  
+This repository is used to create an archive of all repositories in the tbep-tech GitHub organization page and upload to Amazon S3 services on a weekly basis.  Motivation for this workflow was created from an rOpenSci blog post on [2022/3/22](https://ropensci.org/blog/2022/03/22/safeguards-and-backups-for-github-organizations/).
 
